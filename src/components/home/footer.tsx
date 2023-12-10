6694b30bc43d170e1f46aace239ff2f6f0cb12ab
WORKSTATION\22065 <2206539077@qq.com>
2023-12-11 00:45:26 +0800

    footer

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,13 +1,13 @@
 // footer.tsx
 const Footer = () => {
   return (
-    <footer>
-      <p className="text-center text-slate-600">Copyright &#169; 2023 www.wanghaoyu.com.cn. All Rights Reserved.</p>
+    <footer style={{ textAlign: 'center', paddingTop: '20px' }}>
+      <p className="text-slate-600">Copyright &#169; 2023 www.wanghaoyu.com.cn. All Rights Reserved.</p>
       {/* Uncomment the following line if needed */}
-      {/* <p className="text-center text-slate-600">工信部备案: <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2020040184号-3</a></p> */}
-      <div className="text-center">
+      {/* <p className="text-slate-600">工信部备案: <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2020040184号-3</a></p> */}
+      <div>
         <a href="https://www.netlify.com">
-          <img src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg" alt="Deploys by Netlify" />
+          <img style={{ display: 'block', margin: 'auto' }} src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg" alt="Deploys by Netlify" />
         </a>
       </div>
     </footer>
