56d7a7e70d07571c6f25ed326cae032516a6b8d2
WORKSTATION\22065 <2206539077@qq.com>
2023-12-11 00:36:09 +0800

    upload release

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <footer>
       <p className="text-center  text-slate-600">Copyright &#169; 2023  www.wanghaoyu.com.cn. All Rights Reserved.</p>
-	  <p className="text-center  text-slate-600">工信部备案: <a href="https://beian.miit.gov.cn/" target="_brank">鲁ICP备2020040184号-3</a></p>
+//	  <p className="text-center  text-slate-600">工信部备案: <a href="https://beian.miit.gov.cn/" target="_brank">鲁ICP备2020040184号-3</a></p>
+<a href="https://www.netlify.com"> <center> <img src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg" alt="Deploys by Netlify"></center> </a> 
     </footer>
   )
 }
