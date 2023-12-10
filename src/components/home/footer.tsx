cc2bde2f1ed21983f29d8e8db3c5e3c6e73da1c3
WORKSTATION\22065 <2206539077@qq.com>
2023-12-11 00:42:24 +0800

    footer

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,14 +1,17 @@
-
-
-
+// footer.tsx
 const Footer = () => {
   return (
     <footer>
-      <p className="text-center  text-slate-600">Copyright &#169; 2023  www.wanghaoyu.com.cn. All Rights Reserved.</p>
-//	  <p className="text-center  text-slate-600">工信部备案: <a href="https://beian.miit.gov.cn/" target="_brank">鲁ICP备2020040184号-3</a></p>
-<a href="https://www.netlify.com"> <center> <img src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg" alt="Deploys by Netlify"></center> </a> 
+      <p className="text-center text-slate-600">Copyright &#169; 2023 www.wanghaoyu.com.cn. All Rights Reserved.</p>
+      {/* Uncomment the following line if needed */}
+      {/* <p className="text-center text-slate-600">工信部备案: <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2020040184号-3</a></p> */}
+      <div className="text-center">
+        <a href="https://www.netlify.com">
+          <img src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg" alt="Deploys by Netlify" />
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
